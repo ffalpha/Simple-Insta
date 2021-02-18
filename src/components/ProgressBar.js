072d2123aa8325fac08a003e465f2e3c86cb9db0
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-18 23:08:56 +0530

    Added fiel

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import useStorage from "../hooks/useStorage";
+
+function ProgressBar() {
+  return <div className="progress-bar"></div>;
+}
+
+export default ProgressBar;
--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,10 +1,35 @@
 import { useState, useEffect } from "react";
 import { projectStorage } from "../firebase/config";
 
+//Hooks mean resuble code
 //responsible for uplaoding
 
 const useStorage = (file) => {
   const [progress, setProgress] = useState(0);
   const [error, setError] = useState(null);
   const [url, setUrl] = useState(null);
+
+  //when file change usefffet fire everytime
+  useEffect(() => {
+    //reffernce
+    const storageRef = projectStorage.ref(file.name);
+    storageRef.put(file).on(
+      "state_changed",
+      (snap) => {
+        let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(percentage);
+      },
+      (err) => {
+        setError(err);
+      },
+      async () => {
+        const url = await storageRef.getDownloadURL();
+        setUrl(url);
+      }
+    );
+  }, [file]);
+
+  return { progress, url, error };
 };
+
+export default useStorage;
