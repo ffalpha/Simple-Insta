4d355e6c2978349f12d69db473eb185b45b760a8
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-18 22:27:35 +0530

    Added uploadform

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Title from "./components/Title";
+import UploadForm from "./components/UploadForm";
 function App() {
   return (
     <div className="App">
       <Title />
+      <UploadForm />
     </div>
   );
 }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,8 @@
 import firebase from "firebase/app";
-import "firebase/auth";
+import "firebase/storage";
+import "firebase/firestore";
 
-const app = firebase.initializeApp({
+firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -10,5 +11,6 @@ const app = firebase.initializeApp({
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASURMENT_ID,
 });
-
-export default app;
+const projectStorage = firebase.storage();
+const projectFireStore = firebase.firestore();
+export { projectStorage, projectFireStore };
