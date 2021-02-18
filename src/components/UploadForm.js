fa723e5fbc6d561fdc940971bf1198ac380cda11
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-18 22:32:35 +0530

    Added upload function

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function UploadForm() {
+  const changeHandler = (e) => {
+    let selected = e.target.files[0];
+    console.log(selected);
+  };
   return (
     <div>
-      <input type="file" />
+      <input type="file" onChange={changeHandler} />
     </div>
   );
 }
