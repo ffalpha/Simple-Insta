98412ad98224c9b2f9d27f8b313022f6424fb88d
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-19 00:49:07 +0530

    Updated title

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,10 +2,10 @@ import React from "react";
 const Title = () => {
   return (
     <div className="title">
-      <h1>Simple insta</h1>
+      <h1>Simple Insta </h1>
       <h2>Your Pictures</h2>
       <p>
-        A simple Photo uploading site build using FIrebase and React. Add Your
+        A simple Photo uploading site build using Firebase and React. Add Your
         Pictures
       </p>
     </div>
