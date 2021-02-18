f71636273ff1757ae57d1383b0b33f0b71b14f7c
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-18 22:18:28 +0530

    Updated gitignore

---
+++ src/components/Title.js
@@ -0,0 +1,17 @@
+import React from "react";
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Simple insta</h1>
+      <h2>Your Pictures</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus alias
+        dolorum, sed eligendi facilis sint iste omnis unde reiciendis minus
+        ullam fuga temporibus optio quidem cumque. Pariatur vitae mollitia
+        consequatur!
+      </p>
+    </div>
+  );
+};
+
+export default Title;
