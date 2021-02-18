6a9b4aed7d38fce38d62e5ab9069934d1032276f
Kalana Mihiranga <kalanam217@gmail.com>
2021-02-18 22:54:57 +0530

    Added custom hook

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 
 function UploadForm() {
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+  const types = ["image/png", "image/jpeg"];
+
   const changeHandler = (e) => {
     let selected = e.target.files[0];
-    console.log(selected);
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setError(null);
+    } else {
+      setFile(selected);
+      setError("Please select an image file(png or jpeg)");
+    }
   };
   return (
     <div>
       <input type="file" onChange={changeHandler} />
+      <span>+</span>
+      <div className="output">
+        {error && <div className="error">{error}</div>}
+        {file && <div>{file.name}</div>}
+      </div>
     </div>
   );
 }
